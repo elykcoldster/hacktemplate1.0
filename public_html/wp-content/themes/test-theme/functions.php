1116dc1a270d67f965a5e54df2966ef894e66f78
Kyle Xiong <kylexiong1993@gmail.com>
2015-07-10 16:04:47 -0400

    Progress
    Day 2

--- public_html/wp-content/themes/test-theme/functions.php
+++ public_html/wp-content/themes/test-theme/functions.php
@@ -22,3 +22,65 @@ function theme_setup() {
 }
 endif;
 add_action('after_setup_theme', 'theme_setup');
+
+function theme_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+ 
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+ 
+    wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+ 
+    if ( is_singular() && wp_attachment_is_image() ) {
+        wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+
+if ( ! function_exists( 'theme_posted_on' ) ) :
+
+function theme_posted_on() {
+    printf( __( 'Posted on <a href="%1$s" title="%2$s"rel="bookmark">
+    		<time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a>
+    		<span class="byline"> by <span class="author vcard">
+    		<a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'test-theme' ),
+        esc_url( get_permalink() ),
+        esc_attr( get_the_time() ),
+        esc_attr( get_the_date( 'c' ) ),
+        esc_html( get_the_date() ),
+        esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+        esc_attr( sprintf( __( 'View all posts by %s', 'shape' ), get_the_author() ) ),
+        esc_html( get_the_author() )
+    );
+}
+endif;
+ 
+function theme_categorized_blog() {
+    if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
+        // Create an array of all the categories that are attached to posts
+        $all_the_cool_cats = get_categories( array(
+            'hide_empty' => 1,
+        ) );
+ 
+        // Count the number of categories that are attached to the posts
+        $all_the_cool_cats = count( $all_the_cool_cats );
+ 
+        set_transient( 'all_the_cool_cats', $all_the_cool_cats );
+    }
+ 
+    if ( '1' != $all_the_cool_cats ) {
+        // This blog has more than 1 category so shape_categorized_blog should return true
+        return true;
+    } else {
+        // This blog has only 1 category so shape_categorized_blog should return false
+        return false;
+    }
+}
+ 
+function theme_category_transient_flusher() {
+    // Like, beat it. Dig?
+    delete_transient( 'all_the_cool_cats' );
+}
+add_action( 'edit_category', 'theme_category_transient_flusher' );
+add_action( 'save_post', 'theme_category_transient_flusher' );
--- public_html/wp-content/themes/test-theme/index.php
+++ public_html/wp-content/themes/test-theme/index.php
@@ -2,6 +2,11 @@
 
 <div id="primary" class="content-area">
     <div id="content" class="site-content">
+	<?php if (have_posts()) : ?>
+    	<?php while (have_posts()) : the_post() ?>
+    		<?php get_template_part('content-aside', get_post_format()); ?>
+    	<?php endwhile; ?>
+    <?php endif; ?>
     </div><!-- #content .site-content -->
 </div><!-- #primary .content-area -->
 
